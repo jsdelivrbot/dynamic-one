838c8a600625b4193a2dcf46902c104dbe7f075c
Jane W <thebakerynest@gmail.com>
2017-08-16 18:22:30 -0700

    Small modification

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World! This is my first Node JS application!')
 })
 
 app.listen(app.get('port'), function() {
